54cf19edee6723c28d0ce9536de14510aaaabbee
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-08-06 14:02:38 +0800

    refactor: add server, web, contracts directories

---
+++ web/src/components/layout/header/Experiences.tsx
@@ -0,0 +1,12 @@
+
+import { ListItem } from './ListItem';
+
+export function Experiences() {
+  return (
+    <>
+      <ListItem href="/buy-me-coffee">Buy Me A Coffee</ListItem>
+      <ListItem href="/mint">Mint NFT</ListItem>
+      <ListItem href="/paymaster-bundler">Paymaster Bundler</ListItem>
+    </>
+  );
+}
