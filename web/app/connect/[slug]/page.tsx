33b688f28a9763eee36653ed07b59a6a9df874bb
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-08-08 18:14:15 +0800

    feat: aa mint support

---
+++ web/app/connect/[slug]/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Head from "next/head";
+import ConnectBtn from "../../../components/ConnectBtn";
+import { useEffect } from "react";
+
+export default function Page({ params }: { params: { slug: string } }) {
+    useEffect(() => {
+        console.log(params.slug);
+    }, []);
+
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-900">
+            <Head>
+                <title>Center Button</title>
+                <meta
+                    name="description"
+                    content="A centered button with Next.js and Tailwind CSS"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+                <ConnectBtn params={params} />
+            </main>
+        </div>
+    );
+}
--- web/components/SendBtn.tsx
+++ web/components/SendBtn.tsx
@@ -42,33 +42,30 @@ export default function ConnectBtn({ params }: { params: { slug: string } }) {
     const { open, close } = useWeb3Modal();
     const { address, isConnecting, isDisconnected } = useAccount();
     const { open: isOpened, selectedNetworkId } = useWeb3ModalState();
-    const { signMessage, signMessageAsync, data: signature } = useSignMessage();
+    const { signMessage, signMessageAsync, data } = useSignMessage();
 
-    async function sign() {
-        const message = `session ID: ${params.slug}`;
-        const data = await signMessageAsync({ message });
-        console.log(data);
-
-        const result = await verifyMessage(signConfig as any, {
-            address: address as `0x${string}`,
-            message: message,
-            signature: data,
-        });
-        console.log(result);
+    async function submit() {
+        const to = `0x6281f95BD27c9d3FF9b72A4bd670554550c7de8f`;
+        const value = "0.001";
+        sendTransaction({ to, value: parseEther(value) });
     }
+
+    const { data: hash, isPending, sendTransaction } = useSendTransaction();
 
     return (
         <>
-            {!signature && <w3m-button />}
-            {!isDisconnected && !signature && (
+            {!hash && <w3m-button />}
+
+            {!isDisconnected && !hash && (
                 <button
                     className="mt-3 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-200 ease-in-out transform hover:scale-105"
-                    onClick={sign}
+                    disabled={isPending}
+                    onClick={submit}
                 >
-                    Sign message
+                    {isPending ? "Confirming..." : "Send"}
                 </button>
             )}
-            {signature && (
+            {hash && (
                 <>
                     <h2 className="text-white">✅</h2>
                     <h2 className="text-white">You may now close the window</h2>
