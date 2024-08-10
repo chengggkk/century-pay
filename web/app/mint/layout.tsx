8277395d0046c5ec4cc4e14eba60b991f9bd2343
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-08-11 00:55:53 +0800

    chore(frontend): fix metadata

--- web/app/mint/layout.tsx
+++ web/app/mint/layout.tsx
@@ -9,8 +9,8 @@ import { config } from "../../config";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Embedded Accounts Getting Started",
-  description: "Embedded Accounts Quickstart Guide",
+  title: "Century Pay",
+  description: "A discord bot that makes onchain payments easy",
 };
 
 // [!region root-layout]
